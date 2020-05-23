427940c456cb9863f2113c43f05db2983ea3dba5
Max Simpson <maxsimpson95@gmail.com>
2020-05-24 07:41:33 +0800

    created basic skeleton for the app

---
+++ app/db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS `maxyourpower`;
+CREATE DATABASE `maxyourpower`;
---
+++ app/routes/api-routes.js
@@ -0,0 +1,45 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Category = require("../models/category.js");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Get all Categorys
+  app.get("/api/all", function(req, res) {
+
+    // Finding all Categorys, and then returning them to the user as JSON.
+    // Sequelize queries are asynchronous, which helps with perceived speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    Category.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+
+  });
+
+  // Add a Category
+  app.post("/api/new", function(req, res) {
+
+    console.log("Category Data:");
+    console.log(req.body);
+
+    Category.create({
+      name: req.body.name,
+      color: req.body.color,
+      created_at: req.body.created_at
+    }).then(function(results) {
+      // `results` here would be the newly created Category
+      res.end();
+    });
+
+  });
+
+};
