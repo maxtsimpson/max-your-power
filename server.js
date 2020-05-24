24b6e411584001e3b6fb29f1f7f1eac5f79e2e37
Max Simpson <maxsimpson95@gmail.com>
2020-05-24 08:02:38 +0800

    added handlebars

--- server.js
+++ server.js
@@ -4,19 +4,25 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
+const express = require("express");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+let app = express();
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("public"));
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
