daf43f16af35b8489127387be9b8ab9ae41083dc
Max Simpson <maxsimpson95@gmail.com>
2020-05-26 13:09:07 +0800

    added timeblock model. set routes to use timeblock and category

--- models/timeblock.js
+++ models/timeblock.js
@@ -6,14 +6,14 @@ const Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
 const sequelize = new Sequelize(process.env.JAWSDB_URL,{dialect: "mysql"});
 
-// Creates a "Category" model that matches up with DB
-const Category = sequelize.define("Category", {
-  name: Sequelize.STRING,
-  color: Sequelize.STRING, //stores the color of the category. could be HEX i.e. FFFFFF or word i.e. white
+// Creates a "Timeblock" model that matches up with DB
+const Timeblock = sequelize.define("Timeblock", {
+  startTime: Sequelize.TIME,
+  endTime: Sequelize.TIME, //stores the color of the Timeblock. could be HEX i.e. FFFFFF or word i.e. white
 });
 
 // Syncs with DB
-Category.sync();
+Timeblock.sync();
 
-// Makes the Category Model available for other files (will also create a table)
-module.exports = Category;
+// Makes the Timeblock Model available for other files (will also create a table)
+module.exports = Timeblock;
