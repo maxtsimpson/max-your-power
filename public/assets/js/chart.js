2867d68c5d3c3a1a6ec5175d23c88ab9030c7534
Lisheng <lishengyu1986@gmail.com>
2020-06-01 11:40:03 +0800

    add doughnut chart and line chart

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,15 +1,69 @@
-var ctx = document.getElementById("myChart");
+// dummy data for mock up purpose
+const categories = {
+  work: 20,
+  sleep: 10,
+  hobby: 5,
+  untracked: 65,
+};
 
-// Add a doughnut chart
-var myDoughnutChart = new Chart(ctx, {
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+const ctx1 = document.getElementById("pieChart");
+const ctx2 = document.getElementById("lineChart");
+
+// add doughnut chart
+const myDoughnutChart = new Chart(ctx1, {
   type: "doughnut",
-  data: data,
-  options: options,
+  data: {
+    labels: Object.keys(categories),
+    datasets: [
+      {
+        label: "Todays Categories",
+        data: Object.values(categories),
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
 
 // Add line chart
-var myLineChart = new Chart(ctx, {
+const myLineChart = new Chart(ctx2, {
   type: "line",
-  data: data,
-  options: options,
+  data: {
+    labels: weekDays,
+    datasets: [
+      {
+        labels: weekDays,
+        backgroundColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
