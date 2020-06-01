121ecf2863969ca2e149d4552103b3cdb695d50f
Lisheng <lishengyu1986@gmail.com>
2020-06-01 13:28:52 +0800

    line chart works

--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -16,6 +16,8 @@ const weekDays = [
   "Sunday",
 ];
 
+const data = [20, 10, 5, 16, 8, 9, 12];
+
 const ctx1 = document.getElementById("pieChart");
 const ctx2 = document.getElementById("lineChart");
 
@@ -52,17 +54,10 @@ const myLineChart = new Chart(ctx2, {
     datasets: [
       {
         labels: weekDays,
-        backgroundColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-        ],
-        borderWidth: 1,
+        data: data,
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        borderWidth: 3,
+        fill: false,
       },
     ],
   },
